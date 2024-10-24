d53ba64dd82abf36f142e2c638090105905bc2c9
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-10-24 09:51:18 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,89 @@
 function HomePage() {
   return (
-    <div className="min-h-screen flex items-center justify-center flex-col p-6">
-      <p className="text-5xl font-bold mb-6">Scrape360</p>
+    <>
+      <div className="min-h-screen flex items-center justify-center flex-col p-6">
+        <p className="text-5xl font-bold mb-6">Scrape360</p>
 
-      <div className="text-left bg-gray-100 p-4 rounded-lg shadow-md w-full max-w-2xl">
-        <h2 className="text-xl font-semibold mb-2">How to Use the API</h2>
-        <p>
-          To scrape reviews from the Career360 website, send a POST request to
-          the following endpoint:
-        </p>
+        <div className="text-left bg-gray-100 p-4 rounded-lg shadow-md w-full max-w-2xl">
+          <h2 className="text-xl font-semibold mb-2">How to Use the API</h2>
+          <p>
+            To scrape reviews from the Career360 website, send a POST request to
+            the following endpoint:
+          </p>
 
-        <div className="bg-black text-white p-4 rounded-lg my-4">
-          <code>POST https://scrape360.vercel.app/api/scrape</code>
+          <div className="bg-black text-white p-4 rounded-lg my-4">
+            <code className="text-wrap text-xs md:text-sm">
+              POST https://scrape360.vercel.app/api/scrape
+            </code>
+          </div>
+
+          <p>The request should include the following JSON payload:</p>
+
+          <div className="bg-gray-200 p-4 rounded-lg my-4">
+            <code>
+              collegeId: 12345, <br />
+              reviewType: Academics, <br />
+              maxPages: 3
+            </code>
+          </div>
+
+          <p>Example response:</p>
+
+          <div className="bg-gray-200 p-4 rounded-lg my-4">
+            <code>
+              content: [ <br />
+              &nbsp;&nbsp;Review 1 text..., <br />
+              &nbsp;&nbsp;Review 2 text... <br />
+              ], <br />
+              status: 200
+            </code>
+          </div>
+
+          <p>Make sure to include the API key in the headers as:</p>
+
+          <div className="bg-gray-200 p-4 rounded-lg my-4">
+            <code>x-api-key: YOUR_API_KEY</code>
+          </div>
+
+          <h3 className="text-lg font-semibold mt-4">
+            About the Request Fields:
+          </h3>
+          <ul className="list-disc list-inside">
+            <li>
+              <strong>collegeId</strong>: The unique identifier for the college.
+            </li>
+            <li>
+              <strong>reviewType</strong>: The type of review you&lsquo;re
+              looking for (e.g., Academics, College Infrastructure, or
+              Placements).
+            </li>
+            <li>
+              <strong>maxPages</strong>: The maximum number of pages to scrape
+              (optional, defaults to 3).
+            </li>
+          </ul>
         </div>
-
-        <p>The request should include the following JSON payload:</p>
-
-        <div className="bg-gray-200 p-4 rounded-lg my-4">
-          <code>
-            collegeId: 12345, <br />
-            reviewType: Academics, <br />
-            maxPages: 3
-          </code>
-        </div>
-
-        <p>Example response:</p>
-
-        <div className="bg-gray-200 p-4 rounded-lg my-4">
-          <code>
-            content: [ <br />
-            &nbsp;&nbsp;Review 1 text..., <br />
-            &nbsp;&nbsp;Review 2 text... <br />
-            ], <br />
-            status: 200
-          </code>
-        </div>
-
-        <p>Make sure to include the API key in the headers as:</p>
-
-        <div className="bg-gray-200 p-4 rounded-lg my-4">
-          <code>x-api-key: YOUR_API_KEY</code>
-        </div>
-
-        <h3 className="text-lg font-semibold mt-4">
-          About the Request Fields:
-        </h3>
-        <ul className="list-disc list-inside">
-          <li>
-            <strong>collegeId</strong>: The unique identifier for the college.
-          </li>
-          <li>
-            <strong>reviewType</strong>: The type of review you&lsquo;re looking
-            for (e.g., Academics, College Infrastructure, or Placements).
-          </li>
-          <li>
-            <strong>maxPages</strong>: The maximum number of pages to scrape
-            (optional, defaults to 3).
-          </li>
-        </ul>
       </div>
-    </div>
+      <div className="w-full bg-gray-100 p-4 flex items-end justify-end flex-col gap-1">
+        <div>
+          Developed by{" "}
+          <a
+            href="https://developerjaskaran.netlify.app/"
+            target="_blank"
+            className="font-bold ml-1"
+          >
+            Jaskaran Singh
+          </a>
+        </div>
+        <div className="font-bold text-sm">
+          <button className="px-3 py-2 rounded-md bg-black text-white">
+            <a href="mailto:dhillonjaskaran4486@gmail.com" target="_blank">
+              Send Email
+            </a>
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
